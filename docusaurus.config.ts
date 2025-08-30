cc0839847e503e9cc24e7ec4a21fb8435500d3a1
MackenzieMolloy <mackenzie.j.molloy@gmail.com>
2025-08-30 18:28:32 +0100

    Updated deployment to custom subdomain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://radiagon-wiki.github.io',
+  url: 'https://wiki.radiagon.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/wiki/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
